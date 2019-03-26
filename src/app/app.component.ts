fc9c1575fcc81673d71ceee41d1db1f98d12b03d
gudshugo <gudshugo@gmail.com>
2019-03-26 16:40:16 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'projetoAngular';
+  url = 'https://seetorontonow.com.br/uploads/2018/01/cn-tower-dusk.jpg';
+  alt = 'Toronto';
+}
