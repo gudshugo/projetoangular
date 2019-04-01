637c4e9b0e0d81fb396395423aa965ce9fe63563
gudshugo <gudshugo@gmail.com>
2019-04-01 17:26:12 -0300

    Continuaçao aulas angular - verificaçao de Resolvers - Aula 6 Atv 5

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,29 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+
 import { Photo } from '../photo/photo';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
 
-  photos : Photo[] = [];
-    
-  constructor(private photoService : PhotoService, private activatedRoute : ActivatedRoute){
+  photos : Photo [] = [];
+  filter : string = ''; 
 
-  }
+  constructor(private activatedRoute : ActivatedRoute){}
   
   ngOnInit() : void{
-    
-    const userName = this.activatedRoute.snapshot.params.userName;
-
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
-
+      this.photos = this.activatedRoute.snapshot.data['photos'];
   }
   
 }
